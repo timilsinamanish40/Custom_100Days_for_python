23f6ab66cfdcdfd9171617a037feff363bea8eaa
timilsinamanish40 <manishtimilsina40@gmail.com>
2024-09-09 07:56:48 +0545

    Day 13 of python

---
+++ Day_13/largest_between_3.py
@@ -0,0 +1,9 @@
+# as we have already done it in Day_5 largest3.py we will be usig new approach here  ie. max() function
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+greatest = max(num1,num2,num3)
+
+print("The largest number is: " ,greatest)
