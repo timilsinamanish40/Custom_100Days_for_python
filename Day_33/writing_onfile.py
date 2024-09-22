8ca2f1877b1cc54f82437a800ab1042c7230daea
timilsinamanish40 <manishtimilsina40@gmail.com>
2024-09-22 19:45:52 +0545

    Day 33 of python

---
+++ Day_33/writing_onfile.py
@@ -0,0 +1,6 @@
+with open("example.txt", "w") as file:
+    file.write("Hello, World!\n")
+    file.write("This is a simple file writing example.\n")
+    file.write("You can write multiple lines of text.\n")
+
+print("Data written to the file successfully.")
